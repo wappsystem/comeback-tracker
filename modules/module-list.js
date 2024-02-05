a8d99ce11e0a400ab36cace041d729ffc495d19a
Gunnar Unger <gunnar.unger@gmail.com>
2024-02-05 17:15:36 +1100

    update

--- modules/module-list.js
+++ modules/module-list.js
@@ -6,10 +6,10 @@
     var m=$vm.module_list;
     var api="wapp";
     //-------------------------------------------------------------------------------------
-    m[p+"ingredients-data"] ={url:H+"/form-grid/ingredients-data.html",Table:"ingredients-comeback",form_module:"ingredients-form",child_stock:"stock-form",task_name:"Ingredients",};                   
-    m[p+"ingredients-form"] ={url:H+"/form-grid/ingredients-form.html",Table:"ingredients-comeback",client_table:"business-comeback",task_name:"Ingredients"};
+    m[p+"products-data"] ={url:H+"/form-grid/products-data.html",Table:"products-comeback",form_module:"products-form",child_stock:"stock-form",task_name:"Products",};                   
+    m[p+"products-form"] ={url:H+"/form-grid/products-form.html",Table:"products-comeback",client_table:"supplier-comeback",task_name:"Products"};
     m[p+"stock-data"] ={url:H+"/form-grid/stock-data.html",Table:"stock-comeback",form_module:"stock-form",task_name:"Stock Transaction"};                   
-    m[p+"stock-form"] ={url:H+"/form-grid/stock-form.html",Table:"stock-comeback",client_table:"ingredients-comeback",task_name:"Stock Transaction"};
+    m[p+"stock-form"] ={url:H+"/form-grid/stock-form.html",Table:"stock-comeback",client_table:"products-comeback",task_name:"Stock Transaction"};
     m[p+"mixture-data"] ={url:H+"/form-grid/mixture-data.html",Table:"mixture-comeback",form_module:"mixture-form",task_name:"Mixture"};                   
     m[p+"mixture-form"] ={url:H+"/form-grid/mixture-form.html",Table:"mixture-comeback",task_name:"Mixture"};
     m[p+"outcomes-data"] ={url:H+"/form-grid/outcomes-data.html",Table:"outcomes-comeback",form_module:"outcomes-form",task_name:"Outcomes"};                   
@@ -18,8 +18,8 @@
     m[p+"scanned-form"] ={url:H+"/form-grid/scanned-form.html",Table:"scanned-comeback",Table2:"mixture-comeback",Table3:"people-comeback",task_name:"Upload data"};
 
     m[p+"panel-setup"]=    	        {url:H+"/panels/setup.html"};
-    m[p+"business-data"] ={url:H+"/form-grid/business-data.html",Table:"business-comeback",form_module:"business-form",task_name:"Business"};                   
-    m[p+"business-form"] ={url:H+"/form-grid/business-form.html",Table:"business-comeback",Table2:"mixture-comeback",Table3:"people-comeback",task_name:"Business"};
+    m[p+"supplier-data"] ={url:H+"/form-grid/supplier-data.html",Table:"supplier-comeback",form_module:"supplier-form",task_name:"Supplier"};                   
+    m[p+"supplier-form"] ={url:H+"/form-grid/supplier-form.html",Table:"supplier-comeback",Table2:"mixture-comeback",Table3:"people-comeback",task_name:"Supplier"};
     m[p+"people-data"] ={url:H+"/form-grid/people-data.html",Table:"people-comeback",form_module:"people-form",task_name:"People"};                   
     m[p+"people-form"] ={url:H+"/form-grid/people-form.html",Table:"people-comeback",task_name:"People"};
 
